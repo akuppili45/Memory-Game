513bcefcdc30cbbd045d921c1e319a75949fb8be
Aditya Kuppili <akuppili45@gmail.com>
2018-08-15 14:41:30 -0700

    switched playerOne's starting turn to true

--- src/store/reducers/player.js
+++ src/store/reducers/player.js
@@ -2,7 +2,7 @@ import { SWITCH_TURN } from "../actionTypes";
 const player = (state={cardsClicked:[], matches: 0, turn: false}, action) => {
     switch(action.type){
         case SWITCH_TURN:
-            return {...action.player, turn: !state.turn}
+            return {...action.player, turn: !action.player.turn}
         default: return state
     }
 }
