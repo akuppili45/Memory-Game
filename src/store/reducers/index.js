995a807e3fa59ce277b20b292cdad61ee9b283cb
Aditya Kuppili <akuppili45@gmail.com>
2018-08-19 22:08:15 -0700

    added comments for redesign

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers  } from 'redux';
 import cards from './cards';
-// import {playerOne, playerTwo} from './player';
-
+//playerOne: {matches: 0, cardOne: :cardId, cardTwo: :secondCardId}
+//clear each time by dispatching and changing the state
+//playerTwo: {matches: 0, cardOne: :cardId, cardTwo: :secondCardId}
+//clear each time by dispatching and changing the state
+//turn: "PLAYER_ONE" also need to switch by dispatching
 const rootReducer = combineReducers({cards});
 export default rootReducer;
